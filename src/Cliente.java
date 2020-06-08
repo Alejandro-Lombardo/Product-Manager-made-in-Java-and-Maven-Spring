406bc232548efed95869efb08769684f25981e7a
Alejandro-Lombardo <alejandro_lombardo@hotmail.com>
2020-06-08 19:01:10 -0300

    Agregado de archivos java en el proyecto.

---
+++ src/Cliente.java
@@ -0,0 +1,18 @@
+
+public class Cliente {
+	private long ID;
+	private String nombre;
+	private String apellido;
+	private String email;
+	private String password;
+	
+	
+	public Cliente(long ID, String nombre, String apellido, String email, String email, String password)
+	{
+		this.ID = ID;
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.email = email;
+		this.password = password;
+	}
+}
