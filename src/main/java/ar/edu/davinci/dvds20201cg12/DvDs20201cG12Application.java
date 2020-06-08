e884ca5fcfa891bcdfb84a0bc63934c794eaf8f8
Alejandro-Lombardo <alejandro_lombardo@hotmail.com>
2020-06-08 20:59:11 -0300

    Agregado de las librerías de Spring en el proyecto.

---
+++ src/main/java/ar/edu/davinci/dvds20201cg12/DvDs20201cG12Application.java
@@ -0,0 +1,13 @@
+package ar.edu.davinci.dvds20201cg12;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DvDs20201cG12Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DvDs20201cG12Application.class, args);
+	}
+
+}
---
+++ src/test/java/ar/edu/davinci/dvds20201cg12/DvDs20201cG12ApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.davinci.dvds20201cg12;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DvDs20201cG12ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
